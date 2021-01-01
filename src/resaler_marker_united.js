ffaced854c764faa75dd0b5c3100ae521041e84b
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-01-01 17:55:28 +0900

    文字列「転売ヤー」の変更とCSS調整

--- src/resaler_marker_united.js
+++ src/resaler_marker_united.js
@@ -1,1 +1,2 @@
 amazon_black_list();
+
---
+++ src/script.js
@@ -0,0 +1,9 @@
+var script01 = function()
+{
+  console.log("(｀・ω・´)ｖvvv");
+};
+
+(function()
+{
+  chrome.browserAction.onClicked.addListener(script01);
+})();
