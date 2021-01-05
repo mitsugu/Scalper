772245520ee174d4215e4d4b2e935c19b55c1a29
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-01-05 15:28:57 +0900

    rename export text file name
    rename export text file name to 'blacklist-<year>-<month>-<day>.json'.

--- src/importExportList.js
+++ src/importExportList.js
@@ -1,10 +1,18 @@
 function exportList() {
-  var strList = localStorage.qd_blacklist_on_amazon;
-  var strListData = strList;
-  var blob=new Blob([strListData],{type:"text/plan"});
-  var link=document.createElement('a');
+  const BASE_NAME = 'blacklist';
+  const EXTENSION_NAME = '.json';
+  let fullDate  = new Date();
+  let strYear   = fullDate.getFullYear().toString();
+  let month     = fullDate.getMonth()+1;
+  let strMonth  = (month>9) ? month.toString() : '0' + month.toString();
+  let day       = fullDate.getDate();
+  let strDay    = (day>9) ? day.toString() : '0'+day.toString();
+  let strList   = localStorage.qd_blacklist_on_amazon;
+  let strListData = strList;
+  let blob=new Blob([strListData],{type:"text/plan"});
+  let link=document.createElement('a');
   link.href=URL.createObjectURL(blob);
-  link.download='blacklist.json';
+  link.download=BASE_NAME+'-'+strYear+'-'+strMonth+'-'+strDay+EXTENSION_NAME;
   link.click();
 }
 
