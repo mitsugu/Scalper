fad6ea0755f7a765d723de118782e4889c877a3f
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-01-11 18:27:45 +0900

    close #11 エクスポート済みテキストファイルblacklist.jsonのマージ機能
    See issue #11 ( https://github.com/mitsugu/Scalper/issues/11 )
    fix issue #11
    close #11

--- src/options.js
+++ src/options.js
@@ -1,3 +1,19 @@
+function mergeScalperList(e) {
+  let gettingItem = browser.storage.local.get("keyScalperList");
+  gettingItem.then((result) => {
+    let strListData = result.keyScalperList;
+    let oldArr = JSON.parse(strListData);
+    let strInputList = document.querySelector("#listofscalper").value;
+    let inputArr = JSON.parse(strInputList);
+    let targetArr = Array.from(new Set(oldArr.concat(inputArr)));
+    browser.storage.local.set(
+      {"keyScalperList": JSON.stringify(targetArr)}
+    );
+  }).catch((error) => {
+    importScalperList(error);
+  });
+}
+
 function importScalperList(e) {
   let ScalperList = document.querySelector("#listofscalper").value;
   browser.storage.local.set(
@@ -6,4 +22,5 @@ function importScalperList(e) {
   e.preventDefault();
 }
 document.querySelector("#form").addEventListener("submit", this.importScalperList);
+document.querySelector("#merge").addEventListener("click", this.mergeScalperList);
 
